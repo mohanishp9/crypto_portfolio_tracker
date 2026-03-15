3ad609f5f7c67611a1aec5f411c900a185b0d5c8
mohanishp9 <mpingale31@gmail.com>
2026-03-15 23:12:36 +0530

    fix cross-site auth cookie

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -68,8 +68,8 @@ const loginUserController = asyncHandler(async (req: Request, res: Response) => 
         const jwtToken = generateToken(user._id.toString());
         res.cookie("token", jwtToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
         res.status(200).json({
