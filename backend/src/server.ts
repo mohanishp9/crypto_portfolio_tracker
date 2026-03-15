e637822acf234c1967548e3fb2648e26047d63a5
mohanishp9 <mpingale31@gmail.com>
2026-03-16 01:01:04 +0530

    portfolio

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://grove-portfolio.onrender.com"
+            "https://grove-portfolio.onrender.com",
         ],
         credentials: true,
     })
