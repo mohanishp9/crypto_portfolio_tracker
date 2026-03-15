5e190e15f9a985dd53d9824c604a1acde897a308
mohanishp9 <mpingale31@gmail.com>
2026-03-15 22:31:32 +0530

    add frontend url to cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: [
+            "http://localhost:5173",
+            "https://grove-portfolio.onrender.com"
+        ],
         credentials: true,
     })
 );
