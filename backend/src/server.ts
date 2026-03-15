03cbced8bb207ff0290a98f586bc50985229de1f
mohanishp9 <mpingale31@gmail.com>
2026-03-15 23:23:01 +0530

    fix cross-site auth cookie

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://grove-portfolio.onrender.com"
+            "grove-crypto-portfolio.vercel.app"
         ],
         credentials: true,
     })
