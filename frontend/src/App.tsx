9a9d5182a9d6374f2e9bcd385d72b3a8f0b3bef9
mohanishp9 <mpingale31@gmail.com>
2026-05-31 16:35:05 +0530

    Routing Fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,14 @@
 import './App.css'
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
-import type { RootState } from "./app/store";
 import Profile from './pages/Profile';
 import LandingPage from './pages/LandingPage';
 
 
 function App() {
-  const { isAuthenticated } = useSelector((state: RootState) => state.auth);
-
-//
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
